462c762f3dcfcde86856ef6cd811cf99aae4e1df
m3gr4l0 <melo.granvillano@gmail.com>
2021-09-30 20:52:46 +0200

    add: collegamento users-products

--- database/migrations/2021_09_30_145317_create_products_table.php
+++ database/migrations/2021_09_30_145317_create_products_table.php
@@ -15,6 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
             $table->string('name', 150);
             $table->text('description')->nullable();
             $table->string('img_path', 255)->nullable();
@@ -22,8 +23,8 @@ class CreateProductsTable extends Migration
             $table->tinyInteger('visibility');
             $table->float('price',5,2);
             $table->timestamps(); 
+            $table->foreign('user_id')->references('id')->on('users');
         });
-
     }
 
     /**
