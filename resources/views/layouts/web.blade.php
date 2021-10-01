4760ed866492c613996589c87fce5dcd92e11f84
Filippo Mazzamuto <filippomazzamuto7@gmail.com>
2021-10-01 13:50:07 +0200

    add structure web app

---
+++ resources/views/layouts/web.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <title>{{ config('app.name', 'Deliveboo') }}</title>
+
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
+
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+</head>
+<body>
+  <div id="app"></div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+/*
+Route::get('/', function () {
+    return view('layouts.web');
+});
+*/
+
+Route::view('/{any}', 'layouts.web')->where('any','.*');
