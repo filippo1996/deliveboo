db672d1b8816b3a3bb7c2e580ce3a5bb1dc93fd7
m3gr4l0 <melo.granvillano@gmail.com>
2021-10-01 19:17:16 +0200

    add: products into table

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -2,8 +2,26 @@
 
 @section('content')
 <div class="container">
-  @foreach ($products as $product)
-    {{ $product->name }}
-  @endforeach
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Desrizione</th>
+        <th scope="col">Ingredienti</th>
+      </tr>
+    </thead>
+        
+    @foreach ($products as $product)
+      <tbody>
+        <tr>
+          <th scope="row">{{ $product->id }}</th>
+          <td>{{ $product->name }}</td>
+          <td>{{ $product->description }}</td>
+          <td>{{ $product->ingredient }}</td>
+        </tr>
+      </tbody>
+    @endforeach
+  </table>
 </div>
 @endsection
