d24b5a2064a563536143eb1096c444ef7a638dfa
m3gr4l0 <melo.granvillano@gmail.com>
2021-10-03 11:28:07 +0200

    edit: buttons

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -35,25 +35,17 @@
           rows="10">{{ old('description', $product->description) }}</textarea>
       </div>
 
-      <button type="submit" class="btn btn-outline-success">Salva modiche</button>
+      <button type="submit" class="btn btn-success">Salva modiche</button>
     </form>
 
-    <div class="text-center mt-5">
-      <a 
-        href="{{ route('products.index') }}" 
-        class="btn btn-outline-secondary">
-          <svg 
-            xmlns="http://www.w3.org/2000/svg" 
-            width="16" 
-            height="16" 
-            fill="currentColor" 
-            class="bi bi-arrow-left" 
-            viewBox="0 0 16 16">
-              <path 
-                fill-rule="evenodd" 
-                d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
-          </svg>
-      </a>
+    <div class="row mt-5">
+      <div class="col-12">
+        <div class="text-center mt-5">
+          <a href="{{ route('products.index') }}">
+            <button type="button" class="btn btn-outline-danger">Annulla</button>
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 @endsection
--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   <div class="container">
     <div class="row ">
-      <div class="col-12 col-md-6">
+      <div class="col-12">
         <div class="card text-center mt-5">
           <h1 class="card-header">{{ $product->name }}</h1>
           <div class="card-body">
@@ -12,14 +12,16 @@
           </div>
         </div>
       </div>
+    </div> 
     
-      <div class="col-12 col-md-6">
+    <div class="row mt-5">
+      <div class="col-12">
         <div class="text-center mt-5">
           <a href="{{ route('products.index') }}" class="btn btn-outline-secondary">
             Torna ai prodotti
           </a>
         </div>
       </div>
-    </div>  
+    </div>
   </div>    
 @endsection
