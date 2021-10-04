9e1a347a949597cde44fc06ead0718e59394cebc
Nicolò <nicocriva1@gmail.com>
2021-10-04 18:08:25 +0200

    add update router

--- resources/js/partials/Footer.vue
+++ resources/js/partials/Footer.vue
@@ -2,57 +2,6 @@
   <div class="bg-dark">
     <div class="container">
       <div class="row justify-content-md-around">
-        <!-- <div class="col-12">
-          <div class="row">
-            <div class="footer_links col-12 col-sm-5 col-lg-2 d-flex flex-column">
-              <h3 class="my-2 text-secondary">
-                Titolo
-              </h3>
-              <ul>
-                <li><a href="" class="text-dark">Link di prova</a></li>
-                <li><a href="" class="text-dark">Link di prova</a></li>
-                <li><a href="" class="text-dark">Link di prova</a></li>
-                <li><a href="" class="text-dark">Link di prova</a></li>
-                <li><a href="" class="text-dark">Link di prova</a></li>
-              </ul>
-            </div>
-            <div class="footer_links col-12 col-sm-5 col-lg-2 d-flex flex-column">
-              <h3 class="my-2 text-secondary">
-                Titolo
-              </h3>
-              <ul>
-                <li><a href="" class="text-dark">Link di prova</a></li>
-                <li><a href="" class="text-dark">Link di prova</a></li>
-                <li><a href="" class="text-dark">Link di prova</a></li>
-                <li><a href="" class="text-dark">Link di prova</a></li>
-                <li><a href="" class="text-dark">Link di prova</a></li>
-              </ul>
-            </div>
-            <div class="footer_links col-12 col-sm-5 col-lg-2 d-flex flex-column">
-              <h3 class="my-2 text-secondary">
-                Titolo
-              </h3>
-              <ul>
-                <li><a href="" class="text-dark">Link di prova</a></li>
-                <li><a href="" class="text-dark">Link di prova</a></li>
-                <li><a href="" class="text-dark">Link di prova</a></li>
-                <li><a href="" class="text-dark">Link di prova</a></li>
-              </ul>
-            </div>
-            <div class="footer_links col-12 col-sm-5 col-lg-2 d-flex flex-column">
-              <h3 class="my-2 text-secondary">
-                Titolo
-              </h3>
-              <ul>
-                <li><a href="" class="text-dark">Link di prova</a></li>
-                <li><a href="" class="text-dark">Link di prova</a></li>
-                <li><a href="" class="text-dark">Link di prova</a></li>
-              </ul>
-            </div>
-          </div>
-        </div> -->
-
-
 
             <div class="col-12 col-md-6">
               <div class="row mt-5">
