8a8597bcbe11119192ad5ba0d0e9274186442983
m3gr4l0 <melo.granvillano@gmail.com>
2021-10-04 19:37:03 +0200

    edit: remove svg add fontawesome

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -44,16 +44,7 @@
                   <td class="text-center">
                     <a href="{{ route('products.edit', ['product'=>$product->id]) }}" class="text-muted">
                       <div class="btn btn-outline-info">
-                        <svg 
-                          xmlns="http://www.w3.org/2000/svg" 
-                          width="16" 
-                          height="16" 
-                          fill="currentColor" 
-                          class="bi bi-pen" 
-                          viewBox="0 0 16 16">
-                            <path 
-                              d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
-                          </svg>
+                        <i class="fas fa-pen"></i>
                       </div>
                     </a>
                   </td>
@@ -61,7 +52,7 @@
                   {{-- Destroy --}}
                   <td class="text-center">
                     <button data-product-delete="{{ $product->id }}" class="btn btn-outline-danger">
-                      <i class="far fa-trash-alt"></i>
+                      <i class="fas fa-trash"></i>
                     </button> 
                   </td>
                 </tr>
