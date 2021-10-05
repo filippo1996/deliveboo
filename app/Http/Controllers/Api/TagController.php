f25d83624dc63b57374abd4087f5165ec52d524c
Filippo Mazzamuto <filippomazzamuto7@gmail.com>
2021-10-05 11:38:06 +0200

    add filter tags

---
+++ app/Http/Controllers/Api/TagController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Tag;
+
+class TagController extends Controller
+{
+    /**
+     * Display the specified resource.
+     *
+     * @param  string  $slug
+     * @return \Illuminate\Http\Response
+     */
+    public function category($slug)
+    {
+        $results = [
+            'success' => false,
+            'results' => null
+        ];
+
+        $restaurants = Tag::where('slug', $slug)->with('users')->first();
+        
+        if($restaurants){
+            $results = [
+                'success' => true,
+                'results' => $restaurants
+            ];
+        }
+
+        return response()->json($results);
+    }
+}
--- resources/js/views/Category.vue
+++ resources/js/views/Category.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    Stai cercando la categoria {{ slug }}
+    <ul v-if="!message">
+      <li v-for="restaurant in restaurants" :key="restaurant.id">
+        {{ restaurant.name }}
+      </li>
+    </ul>
+    <h3 v-else>{{ message }}</h3>
   </div>
 </template>
 
@@ -9,6 +14,27 @@ export default {
   name: 'Category',
   props: {
     slug: String
+  },
+  data(){
+    return {
+      restaurants: [],
+      url: '/api/category/',
+      message: undefined
+    }
+  },
+  mounted(){
+    this.callApi();
+  },
+  methods: {
+    async callApi(){
+      let response = await axios.get(this.url + this.slug);
+      if(response.data.success){
+        this.restaurants = response.data.results.users;
+        if(!this.restaurants.length) this.message = 'Nessun ristorante trovato';
+      } else {
+        alert('Categoria non trovata');
+      }
+    }
   }
 }
 </script>
