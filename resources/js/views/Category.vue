533b403b17c65bf39c2e6d502784f3f848c2329d
Filippo Mazzamuto <filippomazzamuto7@gmail.com>
2021-10-05 10:18:21 +0200

    add category components

---
+++ resources/js/views/Category.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Stai cercando la categoria {{ slug }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Category',
+  props: {
+    slug: String
+  }
+}
+</script>
+
+<style>
+
+</style>
