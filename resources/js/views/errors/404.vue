3b10e2fda1438c885267f3458441dc2d9f03917d
m3gr4l0 <melo.granvillano@gmail.com>
2021-10-06 18:51:04 +0200

    add: 404 responsive

--- resources/js/views/errors/404.vue
+++ resources/js/views/errors/404.vue
@@ -1,30 +1,39 @@
 <template>
-  <div class="not_found">
-    <div class="background">
+  <div class="background_color">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="not_found">
+            <div class="content">
+              <h1 class="fs-2 mb-1 pt-1 pt-3">Oops! Non abbiamo trovato questa pagina.</h1>
 
-      <h1 class="fs-2 pt-1 mb-1 pt-5">Oops! Non abbiamo trovato questa pagina.</h1>
+              <img src="images/not_found.png" class="img-fluid" alt="Pagina non trovata">
 
-      <img src="images/not_found.png" alt="">
+              <div class="mt-2">
+                <h2 class="fs-3">Forse puoi trovare quello che cerchi qui?</h2>
+              </div>
 
-      <div class="mt-2">
-        <h2 class="fs-3">Forse puoi trovare quello che cerchi qui?</h2>
+              <div class="d-flex justify-content-center mt-2 pb-5">
+                <ul class="d-flex list-unstyled">
+                  <li class="fs-6 p-3">
+                    <a href="localhost:8000" class="text-reset text-decoration-none">Deliveboo</a>
+                  </li>
+
+                  <li class="fs-6 p-3">
+                    <a href="" class="text-reset text-decoration-none">Contattaci</a>
+                  </li>
+
+                  <li class="fs-6 p-3">
+                    <a href="" class="text-reset  text-decoration-none">Chi Siamo</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div class="d-flex justify-content-center mt-2">
-        <ul class="d-flex list-unstyled">
-          <li class="p-3">
-            <a href="localhost:8000" class="text-reset text-decoration-none">Deliveboo</a>
-          </li>
-          <li class="p-3">
-            <a href="" class="text-reset text-decoration-none">Contattaci</a>
-          </li>
-          <li class="p-3">
-            <a href="" class="text-reset  text-decoration-none">Chi Siamo</a>
-          </li>
-        </ul>
-      </div>
+    </div>
   </div>
-</div>
 </template>
 
 
@@ -36,20 +45,21 @@ export default {
 
 
 <style lang="scss" scoped>
+@import 'resources/sass/_variables.scss'; 
 
-.not_found {
-  color: #334e62;
-
-  .background {
+.background_color {
   background-color: rgb(255, 214, 112);
-  text-align: center;
-  object-fit: cover;
 
-    img {
-      width: 800px;
+  .not_found {
+  color: #334e62;
+  
+    .content {
+      text-align: center;
+
+      img {
+        width: 620px;
+      }
     }
-
   }
 }
-
 </style>
