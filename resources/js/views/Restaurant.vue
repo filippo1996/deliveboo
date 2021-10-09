7ff2070c9f70065928786322da1d1aee6b148d0a
m3gr4l0 <melo.granvillano@gmail.com>
2021-10-09 16:03:20 +0200

    css organisation

--- resources/js/views/Restaurant.vue
+++ resources/js/views/Restaurant.vue
@@ -6,7 +6,7 @@
         <div class="col-12 col-lg-9" :class="{'col-lg-12': !cart?.length}">
           
           <!-- Restaurant Header -->
-          <div class="row row-ristorante text-white my-3 py-5">
+          <div class="row restaurant_header text-white my-3 py-5">
             <h1>{{ restaurant.name }}</h1>
             <span>Contatta: {{ restaurant.phone_number }}</span>
           </div>
@@ -32,8 +32,8 @@
                         min="1"
                         class="w-25 h-auto border border rounded_5">
                       <div class="float-lg-end ">{{ product.price.toFixed(2) }} &euro; 
-                        <span class="cart" @click="insertCart(product)">
-                          <i class="fas fa-plus big-icon"></i>
+                        <span class="cursor_pointer" @click="insertCart(product)">
+                          <i class="fas fa-plus plus_icon"></i>
                         </span>
                       </div>
                     </div>
@@ -46,9 +46,10 @@
 
 
         <!-- Cart button -->
-        <div class="fixed-bottom text-center mb-4 d-lg-none">
+        <!-- <div class="fixed-bottom text-center mb-4 d-lg-none">
           <a href="#" class="cart-button px-4 py-3 rounded-pill text-decoration-none">Carrello</a>
-        </div>
+        </div> -->
+        <!-- Da implementare flottante -->
 
 
         <!-- Cart -->
@@ -63,18 +64,18 @@
               <div class="card-body">
                 <div class="row my-2 pb-2" v-for="(obj, index) in cart" :key="index">
                   <div class="row">
-                    <div class="col-2 fw-bold n-carrello">{{ obj.qty }}x</div>
+                    <div class="col-2 fw-bold qty_cart">{{ obj.qty }}x</div>
                     <div class="col-7 text-start ">{{ obj.item.name }}</div>
-                    <div class="col-3 price">{{ obj.item.price.toFixed(2) }} &euro;</div>
+                    <div class="col-3 price">{{ obj.item.price.toFixed(2) }}&euro;</div>
                   </div>
                   <div class="row p-2">
                     <div class="col-md-6 d-flex justify-content-start">
-                      <span class="cart" @click="setQuantity(obj, '-')">
+                      <span class="cursor_pointer" @click="setQuantity(obj, '-')">
                         <i class="fas fa-minus"></i>
                       </span>
                     </div>
                     <div class="col-md-6 d-flex justify-content-end mb-4">
-                      <span class="cart" @click="setQuantity(obj, '+')">
+                      <span class="cursor_pointer" @click="setQuantity(obj, '+')">
                         <i class="fas fa-plus"></i>
                       </span>
                     </div>
@@ -85,7 +86,7 @@
               </div>
 
               <div class="card-footer text-center bg-white mt-2">
-                <h3 class="fw-bold fs-5">Totale carrello {{ totalPriceCart.toFixed(2) }} &euro;</h3>
+                <h3 class="fw-bold fs-5">Totale carrello {{ totalPriceCart.toFixed(2) }}&euro;</h3>
               </div>
 
             </div>
@@ -96,9 +97,19 @@
         <!-- Home & Return to Top -->
         <div class="row mt-5">
           <div class="col-lg-9 d-flex justify-content-center mb-5" :class="{'col-lg-12': !cart?.length}">
-            <router-link class="home px-3" :to="{ name: 'home' }"><i class="fas fa-home"></i> {{ 'Torna alla home' }}</router-link>
+            <router-link 
+              class="to_home_btn px-3" 
+              :to="{ name: 'home' }">
+                <i class="fas fa-home"></i> {{ 'Torna alla home' }} 
+            </router-link>
+
           </div>
-          <a class="freccia text-center" @click="returnToTop" id="returnBtn"><i class="fas fa-arrow-up"></i></a>
+          <a 
+            class="arrow_up text-center" 
+            @click="returnToTop" 
+            id="returnBtn">
+              <i class="fas fa-arrow-up"></i>
+          </a>
         </div>
       </div>
     </div>
@@ -169,141 +180,35 @@ export default {
 <style lang="scss" scoped>
 @import 'resources/sass/_variables.scss';
 
-section{
-background: #ffd670;
+
+// General
+
+section {
+  background: #ffd670;
 }
 
-.card {
-  box-shadow: 8.0px 16.0px 16.0px hsl(0deg 0% 0% / 0.25);
-}
-input{
-  &.rounded_5 {
-    border: 1px solid #000;
-    border-radius: 5px;
-  }
-  
-  &:focus{
-    outline: $blue solid 1px!important;
-    box-shadow:inset 0 1px 3px $blue;
-    -moz-box-shadow:inset 0 1px 3px $blue;
-    -webkit-box-shadow:inset 0 1px 3px $blue;
-  }
-}
-
-.card-title{
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  overflow: hidden;
-}
-#prodotto{
-  border-radius: 15px;
-  height: 260px;
-}
-.descrizione{
-  height: 100px;
-  overflow: auto;
-
-  &::-webkit-scrollbar {
-  width: 5px;
-  }
- 
-  &::-webkit-scrollbar-track {
-  box-shadow: inset 0 0 6px #ffd670;
-  }
- 
-  &::-webkit-scrollbar-thumb {
-  background-color: $light-blue;
-  outline: 1px solid $light-blue;
-  }
-}
-
-.img-box{
-  .card-img{
-    width: 100px; 
-    height: 100px;
-    -webkit-border-radius: 50%; 
-    -webkit-background-clip: padding-box; 
-    -moz-border-radius: 50%; 
-    -moz-background-clip: padding; 
-    border-radius: 50%; 
-    background-clip: padding-box; 
-    object-fit: cover;
-  } 
-}
-
-.big-icon{
-  font-size: 15px;
-  color: $blue;
-  background-color: $light-blue;
-  padding: 8px 10px;
-  border-radius: 50%;
-  margin-left: 20px;
-}
-
-.cart-button{
-  background-color: $light-blue;
-  color: white;
-}
-
-#carrello {
-  position: fixed;
-  right: 0;
-  transform: translateX(-22px);
-  
-  .card {
-    border-radius: 15px;
-    height: 85vh;
-    
-
-    .card-body {
-      overflow-y: auto;  
-    }
-    }
-}
-
-.n-carrello, .price{
-  font-size: 12px;
-}
-
-.lista{
-  border-bottom: 1px solid lightgray;
-}
-
-.row-ristorante{
-  background-image: url("https://wallpaperaccess.com/full/462778.jpg");
-}
-
-.order-button{
-  background-color: $light-blue;
-  padding: 2px 15px;
-
-  &:hover{
-    background-color: #70a3d6;
-  }
-}
-
-.home{
-  background-color: $light-blue;
-  padding: 10px;
-  border-radius: 20px;
-  text-decoration: none;
-}
-
-.cart{
-  cursor: pointer;
-}
-
-.d_none{
+.d_none {
   display: none !important;
 }
 
+.cursor_pointer {
+  cursor: pointer;
+}
 
+.restaurant_header {
+  background-image: url("https://wallpaperaccess.com/full/462778.jpg");
+}
+
+
+
+// Botton
 
 #returnBtn {
   left: 0;
   transform: translateX(22px);
 }
-.freccia{
+
+.arrow_up {
   // display: none;
   position: fixed;
   height: 40px;
@@ -315,8 +220,142 @@ input{
   color: $blue;
   border-radius: 50%;
   transition: ease all 0.3s;
-    &:hover{
-      transform: scale(1.5);
-    }
+
+  &:hover {
+    transform: scale(1.5);
+  }
 }
+
+.to_home_btn {
+  background-color: $light-blue;
+  padding: 10px;
+  border-radius: 20px;
+  text-decoration: none;
+}
+
+
+
+// Product
+
+#prodotto {
+  border-radius: 15px;
+  height: 260px;
+}
+
+.card {
+  box-shadow: 8.0px 16.0px 16.0px hsl(0deg 0% 0% / 0.25);
+
+  .img-box {
+    .card-img {
+      width: 100px; 
+      height: 100px;
+      -webkit-border-radius: 50%; 
+      -webkit-background-clip: padding-box; 
+      -moz-border-radius: 50%; 
+      -moz-background-clip: padding; 
+      border-radius: 50%; 
+      background-clip: padding-box; 
+      object-fit: cover;
+    } 
+  }
+
+  .card-title {
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+
+  input {
+    &.rounded_5 {
+      border: 1px solid #000;
+      border-radius: 5px;
+    }
+  
+    &:focus {
+      outline: $blue solid 1px!important;
+      box-shadow:inset 0 1px 3px $blue;
+      -moz-box-shadow:inset 0 1px 3px $blue;
+      -webkit-box-shadow:inset 0 1px 3px $blue;
+    }
+  }
+
+  .descrizione {
+    height: 100px;
+    overflow: auto;
+
+    &::-webkit-scrollbar {
+      width: 5px;
+    }
+ 
+    &::-webkit-scrollbar-track {
+      box-shadow: inset 0 0 6px #ffd670;
+    }
+ 
+    &::-webkit-scrollbar-thumb {
+      background-color: $light-blue;
+      outline: 1px solid $light-blue;
+    }
+  }
+
+  .plus_icon {
+    font-size: 15px;
+    color: $blue;
+    background-color: $light-blue;
+    padding: 8px 10px;
+    border-radius: 50%;
+    margin-left: 20px;
+  }
+}
+
+
+
+// Cart
+
+#carrello {
+  position: fixed;
+  right: 0;
+  transform: translateX(-22px);
+  
+  .card {
+    border-radius: 15px;
+    height: 85vh;
+
+    .card-body {
+      font-size: 11px;
+      overflow-y: auto; 
+      
+      .qty_cart, 
+      .price {
+        font-size: 12px;
+      } 
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Da implementare
+
+// .cart-button {
+//   background-color: $light-blue;
+//   color: white;
+// }
+
+// .order-button{
+//   background-color: $light-blue;
+//   padding: 2px 15px;
+
+//   &:hover{
+//     background-color: #70a3d6;
+//   }
+// }
 </style>
