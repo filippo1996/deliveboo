84f9ccb26580f7da8dfc2d2f25c48e6c03ec0223
Nicolò <nicocriva1@gmail.com>
2021-10-13 19:43:00 +0200

    add sezione mail

--- resources/views/emails/order_confirm.blade.php
+++ resources/views/emails/order_confirm.blade.php
@@ -7,8 +7,30 @@
     <title>Document</title>
   </head>
   <body>
-    
-    <h1>Ordine confermato</h1>
+    <div class="bg-primary">
+      <h1>Ordine confermato</h1>
+      <div>Il cliente xxx ha appena effettuato un'ordine.</div>
+      <div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+            Prodotto
+            <span class="badge bg-primary rounded-pill">14</span>
+          </li>
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+            Prodotto
+            <span class="badge bg-primary rounded-pill">2</span>
+          </li>
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+            Prodotto
+            <span class="badge bg-primary rounded-pill">5</span>
+          </li>
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+            Prodotto
+            <span class="badge bg-primary rounded-pill">3</span>
+          </li>
+        </ul>
+      </div>
+    </div>
 
 </body>
 </html>
