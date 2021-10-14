0206c8e5cfe9a39915826f1379fa2d4c8eeccd6f
Nicolò <nicocriva1@gmail.com>
2021-10-14 16:11:04 +0200

    add correzioni mail

--- resources/views/emails/order_confirm.blade.php
+++ resources/views/emails/order_confirm.blade.php
@@ -4,13 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+      body { font-family:verdana; }
+      h1 { text-align: center; margin-bottom: 20px; }
+      ul { list-style: none; }
+      li { margin-bottom: 10px; }
+    </style>
     <title>Document</title>
   </head>
   <body>
+
     <div class="bg-primary">
       <h1>Ordine confermato</h1>
-      <div>Il cliente xxx ha appena effettuato un'ordine.</div>
+      <div>Il cliente xxx ha appena effettuato un'ordine:</div>
       <div>
         <ul class="list-group list-group-flush">
           <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -32,9 +38,6 @@
         </ul>
       </div>
     </div>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
 </html>
