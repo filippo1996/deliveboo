e1070841fa575e89a65a23b6f0bc2b821f2b6ec0
Filippo Mazzamuto <filippomazzamuto7@gmail.com>
2021-10-17 18:48:32 +0200

    add show product in the order backend

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -20,46 +20,6 @@ class OrderController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        // Validazione dei Dati
-        /*
-        $request->validate([
-            'name' => 'required|max:150',
-            'lastname' => 'required',
-            'email' => 'required',
-            'prcie' => 'required|in:0,1',
-            'phone_number' => 'required',
-            'address' => 'required',
-            'house_number' => 'required',   
-            'postal_code' => 'required',   
-            'city' => 'required'   
-        ]);
-
-        // Auth::user()->id;
-
-        // Prendo i dati
-        $data = $request->all();
-        return redirect()->route('orders.index');
-        */
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param  \App\Models\Order  $order
@@ -68,40 +28,8 @@ class OrderController extends Controller
     public function show(Order $order)
     {
         $this->authorize('view', $order);
+        
         return view('order.show', compact('order'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Order  $order
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Order $order)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Order  $order
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Order $order)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Order  $order
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Order $order)
-    {
-        //
-    }
 }
--- resources/views/order/show.blade.php
+++ resources/views/order/show.blade.php
@@ -6,10 +6,6 @@
       <div class="col-12">
         <div class="card text-center w-50 mt-5 m-auto">
 
-          <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
-          <!-- <h1 class="card-header">{{ $order->name }}</h1>
-          <h2 class="card-header">{{ $order->lastname }}</h2> -->
-
           <h1 class="card-header">Ordine N.{{ $order->id }}</h1>
           <h3 class="card-header">Data ordine: {{ $order->created_at }}</h3>
           <div class="card-body p-3">
@@ -24,11 +20,7 @@
                 <span>{{ $order->city }},</span>
                 <span>{{ $order->postal_code }}</span>
               </p>
-              <p class="card-text">Prezzo ordine: {{ $order->price }}€</p>
-
-              <!-- <p class="card-text">{{ $order->house_number }}</p>
-              <p class="card-text">{{ $order->postal_code }}</p>
-              <p class="card-text">{{ $order->city }}</p> -->
+              <p class="card-text">Prezzo ordine: {{ $order->price }} &euro;</p>
 
             </div>
           </div>
@@ -36,7 +28,15 @@
           <h3 class="card-header">Elenco prodotti ordinati</h3>
           <div class="card-body p-3">
             <div class="text-start p-2">
-              <p class="card-text">Qui va l'elenco dei prodotti ordinati</p>
+              <ol>
+                @foreach ($order->products as $item)
+                    <li>
+                      <img src="{{ $item->img_path }}" alt="{{ $item->name }}" width="60px">
+                      {{ $item->name }} |
+                      <span><strong>Quantità: {{ $item->pivot->qty }}</strong></span>
+                    </li>
+                @endforeach
+              </ol>
             </div>
           </div>
 
