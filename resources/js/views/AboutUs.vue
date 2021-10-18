dadad3aa075bfa22d95dae669fdbec85c59270da
m3gr4l0 <melo.granvillano@gmail.com>
2021-10-18 23:31:29 +0200

    add: about responsive

--- resources/js/views/AboutUs.vue
+++ resources/js/views/AboutUs.vue
@@ -1,15 +1,15 @@
 <template>
   <section class="mt-5">
     <div class="background_color">
-      <div class="container pt-5">
+      <div class="container pt-5 m-auto">
          <!-- Riga 1 -->
-        <div class="row mt-5 p-5">
-          <div class="col-6 d-flex">
+        <div class="row mt-5">
+          <div class="col-12 col-md-8 col-lg-6 d-flex">
             <p class="d-flex align-items-center">Mi ritengo una persona abbastanza curiosa e con tanta voglia di imparare. Sono sempre pronto a mettermi in gioco per apprendere e imparare tutto ciò che riguarda il mondo della tecnologia e non solo.
             Questa mia caratteristica, negli anni, mi ha permesso di perfezionare sempre di più il mio modo di lavorare.</p>
           </div>
-          <div class="col-6 text-center">
-            <img src="images/us/fil.jpg" alt="fil" class="circle">
+          <div class="col-12 col-md-4 col-lg-6 text-center">
+            <img src="images/us/fil.jpg" alt="fil" class="circle img-fluid">
             <div class="fs-3 mt-1">
               <a href="https://www.linkedin.com/in/filippo-mazzamuto/" class="text-reset text-decoration-none">
                 <i class="fab fa-linkedin px-2"></i>
@@ -26,9 +26,9 @@
         
 
         <!-- Riga 2 -->
-        <div class="row mt-5 p-5">
-          <div class="col-6 text-center">
-            <img src="images/us/fraJ.jpg" alt="fraJ" class="circle">
+        <div class="row mt-5 py-3">
+          <div class="col-12 col-md-8 col-lg-6 text-center">
+            <img src="images/us/fraJ.jpg" alt="fraJ" class="circle img-fluid">
             <div class="fs-3 mt-1">
               <a href="https://www.linkedin.com/in/francesco-pittiglio-806769221/" class="text-reset text-decoration-none">
                 <i class="fab fa-linkedin px-2"></i>
@@ -41,19 +41,19 @@
               </a>
             </div>
           </div>
-          <div class="col-6 d-flex">
+          <div class="col-12 col-md-4 col-lg-6 d-flex">
             <p class="d-flex align-items-center">Da sempre appassionato di tecnologia e programmazione, mi riservo anche del tempo per passeggiate in montagna sempre in cerca di nuove esperienze.</p>
           </div>
         </div>
 
 
         <!-- Riga 3 -->
-        <div class="row mt-5 p-5">
-          <div class="col-6 d-flex">
+        <div class="row mt-5 py-3">
+          <div class="col-12 col-md-8 col-lg-6 d-flex">
             <p class="d-flex align-items-center">Sono un ragazzo di 23 anni . Appassionato da sempre del mondo informatico e quello dell'economia, alla continua ricerca di stimoli per la crescita personale. Sono energico e solare ho un’ottima attitudine al lavoro in team. Ho un forte spirito di gruppo e di adattamento grazie all’esperienza acquisita in ambienti multiculturali e lavorativi, sempre disposto a mettermi in gioco e lanciarmi in nuove sfide.</p>
           </div>
-          <div class="col-6 text-center">
-            <img src="images/us/fra.jpg" alt="fra" class="circle">
+          <div class="col-12 col-md-4 col-lg-6 text-center">
+            <img src="images/us/fra.jpg" alt="fra" class="circle img-fluid">
             <div class="fs-3 mt-1">
               <a href="https://www.linkedin.com/in/francesco-coclite-web-dev/" class="text-reset text-decoration-none">
                 <i class="fab fa-linkedin px-2"></i>
@@ -70,9 +70,9 @@
 
 
         <!-- Riga 4 -->
-        <div class="row mt-5 p-5">
-          <div class="col-6 text-center">
-            <img src="images/us/nico.jpg" alt="nico" class="circle">
+        <div class="row mt-5 py-3">
+          <div class="col-12 col-md-8 col-lg-6 text-center">
+            <img src="images/us/nico.jpg" alt="nico" class="circle img-fluid">
             <div class="fs-3 mt-1">
               <a href="https://www.linkedin.com/in/nicol%C3%B2-crivaro/" class="text-reset text-decoration-none">
                 <i class="fab fa-linkedin px-2"></i>
@@ -85,7 +85,7 @@
               </a>
             </div>
           </div>
-          <div class="col-6 d-flex">
+          <div class="col-12 col-md-4 col-lg-6 d-flex">
             <p class="d-flex align-items-center">Sono un ragazzo appassionato di tecnologia, attento ai dettagli e sempre pronto ad imparare.
             Voglio approfondire la mia conoscenza nei linguaggi di programmazione e mettermi in gioco nel mondo dello sviluppo web.</p>
           </div>
@@ -93,12 +93,12 @@
 
 
         <!-- Riga 5 -->
-        <div class="row mt-5 p-5">
-          <div class="col-6 d-flex">
+        <div class="row mt-5 pt-3 pb-5">
+          <div class="col-12 col-md-8 col-lg-6 d-flex">
             <p class="d-flex align-items-center">Curioso e tech addicted. Se non scrivo codice, allora leggo. Adoro passeggiare immerso nella natura.</p>
           </div>
-          <div class="col-6 text-center">
-            <img src="images/us/melo.png" alt="melo" class="circle">
+          <div class="col-12 col-md-4 col-lg-6 text-center">
+            <img src="images/us/melo.png" alt="melo" class="circle img-fluid">
             <div class="fs-3 mt-1">
               <a href="https://www.linkedin.com/in/carmelo-granvillano/" class="text-reset text-decoration-none">
                 <i class="fab fa-linkedin px-2"></i>
@@ -144,5 +144,14 @@ img {
   border-radius: 50%;
 }
 
+p {
+  min-width: 200px;
+}
+
+@media screen and (min-width: 0px) and (max-width: 575px) {
+  p {
+    display: none!important;
+  }
+}
 
 </style>
