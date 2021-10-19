395071fb897791d5c03dd9389ae55302b2fbad4f
Nicolò <nicocriva1@gmail.com>
2021-10-19 12:23:28 +0200

    add modifiche

--- resources/js/views/AboutUs.vue
+++ resources/js/views/AboutUs.vue
@@ -9,21 +9,23 @@
             Questa mia caratteristica, negli anni, mi ha permesso di perfezionare sempre di più il mio modo di lavorare.</p>
           </div>
           <div class="col-12 col-md-4 col-lg-6 text-center">
-            <h4 class="name d-flex justify-content-center mb-3">Filippo Mazzamuto</h4>
+            <h4 class="d-flex justify-content-center mb-3">Filippo Mazzamuto</h4>
             <img src="images/us/fil.jpg" alt="fil" class="circle img-fluid">
             <div class="fs-3 mt-1">
-              <a href="https://www.linkedin.com/in/filippo-mazzamuto/" target="_blank" class="text-reset text-decoration-none">
+              <a href="https://www.linkedin.com/in/filippo-mazzamuto/" target="_blank" class="text-reset icon text-decoration-none">
                 <i class="fab fa-linkedin px-2"></i>
               </a>
-              <a href="https://github.com/filippo1996" target="_blank" class="text-reset text-decoration-none">
+              <a href="https://github.com/filippo1996" target="_blank" class="text-reset icon text-decoration-none">
                 <i class="fab fa-github-square px-2"></i>
               </a>
-              <a href="http://instagram.com/_u/filippo_mazzamuto" class="text-reset text-decoration-none">
-                <i class="fab fa-instagram-square px-2"></i>
-              </a>
-              <!-- <a href="https://www.instagram.com/filippo_mazzamuto" class="text-reset text-decoration-none">
+              <!--
+                ---------- aprire l'applicazione di instagram ---------  
+              <a href="http://instagram.com/_u/filippo_mazzamuto" class="text-reset icon text-decoration-none">
                 <i class="fab fa-instagram-square px-2"></i>
               </a> -->
+              <a href="https://www.instagram.com/filippo_mazzamuto" class="text-reset icon text-decoration-none">
+                <i class="fab fa-instagram-square px-2"></i>
+              </a>
             </div>
           </div>
         </div>
@@ -32,17 +34,17 @@
         <!-- Riga 2 -->
         <div class="row mt-5 py-3">
           <div class="col-12 col-md-4 col-lg-6 text-center">
-            <h4 class="name d-flex justify-content-center mb-3">Francesco Pittiglio</h4>
+            <h4 class="d-flex justify-content-center mb-3">Francesco Pittiglio</h4>
             <img src="images/us/fraJ.jpg" alt="fraJ" class="circle img-fluid">
             <div class="fs-3 mt-1">
-              <a href="https://www.linkedin.com/in/francesco-pittiglio-806769221/" target="_blank" class="text-reset text-decoration-none">
+              <a href="https://www.linkedin.com/in/francesco-pittiglio-806769221/" target="_blank" class="text-reset icon text-decoration-none">
                 <i class="fab fa-linkedin px-2"></i>
               </a>
-              <a href="https://github.com/PittiglioFrancesco" target="_blank" class="text-reset text-decoration-none">
+              <a href="https://github.com/PittiglioFrancesco" target="_blank" class="text-reset icon text-decoration-none">
                 <i class="fab fa-github-square px-2"></i>
               </a>
-              <a href="#" class="text-reset text-decoration-none">
-                <i class="fab fa-twitter-square px-2"></i>
+              <a href="https://www.instagram.com/_chillin_frank_" class="text-reset icon text-decoration-none">
+                <i class="fab fa-instagram-square px-2"></i>
               </a>
             </div>
           </div>
@@ -58,17 +60,17 @@
             <p class="d-flex align-items-center text-end">Sono un ragazzo di 23 anni . Appassionato da sempre del mondo informatico e quello dell'economia, alla continua ricerca di stimoli per la crescita personale. Sono energico e solare ho un’ottima attitudine al lavoro in team. Ho un forte spirito di gruppo e di adattamento grazie all’esperienza acquisita in ambienti multiculturali e lavorativi, sempre disposto a mettermi in gioco e lanciarmi in nuove sfide.</p>
           </div>
           <div class="col-12 col-md-4 col-lg-6 text-center">
-            <h4 class="name d-flex justify-content-center mb-3">Francesco Coclite</h4>
+            <h4 class="d-flex justify-content-center mb-3">Francesco Coclite</h4>
             <img src="images/us/fra.jpg" alt="fra" class="circle img-fluid">
             <div class="fs-3 mt-1">
-              <a href="https://www.linkedin.com/in/francesco-coclite-web-dev/" target="_blank" class="text-reset text-decoration-none">
+              <a href="https://www.linkedin.com/in/francesco-coclite-web-dev/" target="_blank" class="text-reset icon text-decoration-none">
                 <i class="fab fa-linkedin px-2"></i>
               </a>
-              <a href="https://github.com/FrancescoCoclite" target="_blank" class="text-reset text-decoration-none">
+              <a href="https://github.com/FrancescoCoclite" target="_blank" class="text-reset icon text-decoration-none">
                 <i class="fab fa-github-square px-2"></i>
               </a>
-              <a href="#" class="text-reset text-decoration-none">
-                <i class="fab fa-twitter-square px-2"></i>
+              <a href="https://www.instagram.com/francescococlite" class="text-reset icon text-decoration-none">
+                <i class="fab fa-instagram-square px-2"></i>
               </a>
             </div>
           </div>
@@ -78,17 +80,17 @@
         <!-- Riga 4 -->
         <div class="row mt-5 py-3">
           <div class="col-12 col-md-4 col-lg-6 text-center">
-            <h4 class="name d-flex justify-content-center mb-3">Nicolò Crivaro</h4>
+            <h4 class="d-flex justify-content-center mb-3">Nicolò Crivaro</h4>
             <img src="images/us/nico.jpg" alt="nico" class="circle img-fluid">
             <div class="fs-3 mt-1">
-              <a href="https://www.linkedin.com/in/nicol%C3%B2-crivaro/" target="_blank" class="text-reset text-decoration-none">
+              <a href="https://www.linkedin.com/in/nicol%C3%B2-crivaro/" target="_blank" class="text-reset icon text-decoration-none">
                 <i class="fab fa-linkedin px-2"></i>
               </a>
-              <a href="https://github.com/nicocriva" target="_blank" class="text-reset text-decoration-none">
+              <a href="https://github.com/nicocriva" target="_blank" class="text-reset icon text-decoration-none">
                 <i class="fab fa-github-square px-2"></i>
               </a>
-              <a href="#" class="text-reset text-decoration-none">
-                <i class="fab fa-twitter-square px-2"></i>
+              <a href="https://www.instagram.com/nicocriva" class="text-reset icon text-decoration-none">
+                <i class="fab fa-instagram-square px-2"></i>
               </a>
             </div>
           </div>
@@ -105,17 +107,17 @@
             <p class="d-flex align-items-center text-end">Curioso e tech addicted. Se non scrivo codice, allora leggo. Adoro passeggiare immerso nella natura.</p>
           </div>
           <div class="col-12 col-md-4 col-lg-6 text-center">
-            <h4 class="name d-flex justify-content-center mb-3">Carmelo Granvillano</h4>
+            <h4 class="d-flex justify-content-center mb-3">Carmelo Granvillano</h4>
             <img src="images/us/melo.png" alt="melo" class="circle img-fluid">
             <div class="fs-3 mt-1">
-              <a href="https://www.linkedin.com/in/carmelo-granvillano/" target="_blank"  class="text-reset text-decoration-none">
+              <a href="https://www.linkedin.com/in/carmelo-granvillano/" target="_blank"  class="text-reset icon text-decoration-none">
                 <i class="fab fa-linkedin px-2"></i>
               </a>
-              <a href="https://github.com/megralo" target="_blank" class="text-reset text-decoration-none">
+              <a href="https://github.com/megralo" target="_blank" class="text-reset icon text-decoration-none">
                 <i class="fab fa-github-square px-2"></i>
               </a>
-              <a href="#" class="text-reset text-decoration-none">
-                <i class="fab fa-twitter-square px-2"></i>
+              <a href="https://www.instagram.com/m3gr4l0" class="text-reset icon text-decoration-none">
+                <i class="fab fa-instagram-square px-2"></i>
               </a>
             </div>
           </div>
@@ -160,22 +162,33 @@ img {
   border-radius: 50%;
 }
 
-.name{
-  color: $blue;
-  font-weight: 500;
-}
-
 p,
 i,
-span {
-  color: #41708E;
+h4 {
+  color: $blue;
+
+}
+
+.icon{
+  i{
+    transition: 0.2s;
+  }
+
+  &:hover i{
+    color: $light-blue;
+  }
+}
+
+
+h4{
+  font-weight: 500;
 }
 
 p {
   min-width: 200px;
 }
 
-@media screen and (min-width: 0px) and (max-width: 575px) {
+@media screen and (min-width: 0px) and (max-width: 767.5px) {
   p {
     display: none!important;
   }
