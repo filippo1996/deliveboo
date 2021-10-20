92f775206a99636fbeaf4090f187eb31dbf1af5e
m3gr4l0 <melo.granvillano@gmail.com>
2021-10-20 08:55:28 +0200

    fix: button in show product

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -6,7 +6,7 @@
       <div class="col-lg-8 m-auto">
         <div class="card text-center w-50 m-auto background_color_y text_colo box_shadows">
           <img class="card-img-top image-auto" src="{{ $product->img_path }}" alt="{{ $product->name }}">
-          <h1 class="card-header fs-4 fw-bold">{{ $product->name }}</h1>
+          <h1 class="card-header fs-5 fw-bold">{{ $product->name }}</h1>
           <div class="card-body p-3">
             {{-- <h2 class="card-title">{{ $product->slug }}</h2> --}}
             <div class="text-start p-2">
@@ -24,22 +24,25 @@
 
             </div>
           </div>
-          <h2 class="card-footer fs-4">{{ $product->price }}€</h2>
+          <h2 class="card-footer fs-5 fw-bold">{{ $product->price }}€</h2>
+          <a href="{{ route('products.edit', ['product'=>$product->id]) }}" class="m-1 mt-0">
+            <div class="btn bottone border-0">
+              <i class="fas fa-pen"></i>
+            </div>
+          </a>
         </div>
       </div>
     </div> 
     
-    <div class="row mt-5">
-      <div class="col-12 mb-5">
-        <div class="text-center mt-5">
-          <a href="{{ route('products.edit', ['product'=>$product->id]) }}" class="btn btn-outline-info">
-            Modifica prodotto
-          </a>
-          <a href="{{ route('products.index') }}" class="btn btn-outline-success">
+    {{-- Bottone ridondante --}}
+    {{-- <div class="row my-5">
+      <div class="col-12">
+        <div class="text-center">
+          <a href="{{ route('products.index') }}" class="btn bottone">
             Torna ai prodotti
           </a>
         </div>
-      </div>
+      </div> --}}
       <!-- <div class="col-6">
         <div class="text-center mt-5">
           <a href="{{ route('products.index') }}" class="btn btn-outline-secondary">
