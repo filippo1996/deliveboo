e307673c8fd19f364fb87dba51de8fd7525eed0f
m3gr4l0 <melo.granvillano@gmail.com>
2021-10-20 09:47:19 +0200

    fix: color show

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -4,10 +4,10 @@
   <div class="container">
     <div class="row mt-5">
       <div class="col-lg-8 m-auto">
-        <div class="card text-center w-50 m-auto background_color_y text_colo box_shadows">
-          <img class="card-img-top img-fluid w-50 m-auto" src="{{ $product->img_path }}" alt="{{ $product->name }}">
-          <h1 class="card-header fs-5 fw-bold">{{ $product->name }}</h1>
-          <div class="card-body p-3">
+        <div class="card text-center w-50 m-auto background_color_y text_color box_shadows">
+          <img class="card-img-top img-fluid w-50 m-auto p-3" src="{{ $product->img_path }}" alt="{{ $product->name }}">
+          <h1 class="card-header fs-5 fw-bold background_color_y">{{ $product->name }}</h1>
+          <div class="card-body p-3 background_color_y">
             {{-- <h2 class="card-title">{{ $product->slug }}</h2> --}}
             <div class="text-start p-2">
               
@@ -24,10 +24,10 @@
 
             </div>
           </div>
-          <h2 class="card-footer fs-5 fw-bold">{{ $product->price }}€</h2>
+          <h2 class="card-footer fs-5 fw-bold background_color_y">{{ $product->price }}€</h2>
           <a href="{{ route('products.edit', ['product'=>$product->id]) }}" class="m-1 mt-0">
             <div class="btn bottone border-0">
-              <i class="fas fa-pen"></i>
+              <i class="fas fa-pen text_color"></i>
             </div>
           </a>
         </div>
