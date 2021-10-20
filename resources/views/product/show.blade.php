533a79ccafd27e25cf0232e90a45c6490763e124
m3gr4l0 <melo.granvillano@gmail.com>
2021-10-20 08:46:43 +0200

    add: graphic show product

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -3,28 +3,28 @@
 @section('content')
   <div class="container">
     <div class="row mt-5">
-      <div class="col-12">
-        <div class="card text-center w-50 mt-5 m-auto">
-          <img class="card-img-top" src="{{ $product->img_path }}" alt="{{ $product->name }}">
-          <h1 class="card-header">{{ $product->name }}</h1>
+      <div class="col-lg-8 m-auto">
+        <div class="card text-center w-50 m-auto background_color_y text_colo box_shadows">
+          <img class="card-img-top image-auto" src="{{ $product->img_path }}" alt="{{ $product->name }}">
+          <h1 class="card-header fs-4 fw-bold">{{ $product->name }}</h1>
           <div class="card-body p-3">
             {{-- <h2 class="card-title">{{ $product->slug }}</h2> --}}
             <div class="text-start p-2">
               
-              <p class="card-text">
-                Descrizione:
+              <p class="card-text fs-6">
+                <span class="fw-bold">Descrizione:</span>  
                 <br>
                 {{ $product->description }}
               </p>
-              <p class="card-text">
-                Ingredienti:
+              <p class="card-text fs-6">
+                <span class="fw-bold">Ingredienti:</span>
                 <br>
                 {{ $product->ingredient }}
               </p>
 
             </div>
           </div>
-          <h1 class="card-header">{{ $product->price }}€</h1>
+          <h2 class="card-footer fs-4">{{ $product->price }}€</h2>
         </div>
       </div>
     </div> 
